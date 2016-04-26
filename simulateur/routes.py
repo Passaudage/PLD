3b38ef80f456f336b41200b8345ed8526a622d6e
paul <compagnonpaul@hotmail.fr>
2016-04-26 14:33:29 +0200

    intermédiair

--- simulateur/routes.py
+++ simulateur/routes.py
@@ -1,18 +1,34 @@
+from enum import Enum
+
 import coordonnees
 import controleAcces
+import vehicule
+
 
 class intersection:
     
 
 class voie:
-    def __init__(self):
+    def __init__(self, direction):
         self.intersectionsAccessibles = []
+        self.direction = direction
+        self.vehicules = []
+
+    def a_de_la_place(self):
+        return
 
     def setTroncon(self, troncon):
         self.troncon = troncon
 
-
 class troncon:
-    def __init__(self, tete, queue):
+    def __init__(self, tete, queue, probabilite_entree, longueur):
         self.tete = tete
         self.queue = queue
+        self.probabilite_entree = probabilite_entree
+        self.longueur = longueur
+        self.voies = []
+
+class direction(Enum):
+    gauche = 1
+    tout_droit = 2
+    droite = 3
--- simulateur/vehicule.py
+++ simulateur/vehicule.py
@@ -3,15 +3,42 @@ import coordonnees
 class vehicule:
     acceleration = coordonnees(0,0)
     vitesse = coordonnees(0,0)
-    def __init__(self, coordonnees, aggressivite, longueur):
+    est_arrete = False
+
+    def __init__(self, coordonnees, aggressivite, longueur, voie, prochaine_direction):
         self.coordonnees = coordonnees
         self.aggressivite = aggressivite
         self.longueur = longueur
         self.changeDirection(0,0)
+        self.voie = voie
+        self.prochaine_direction = prochaine_direction
+        self.vehicule_precedent
+        self.vehicule_suivant = []
     
     def changeDirection(self, x, y):
         self.direction = coordonnees(x, y)
 
     def avancerBoucle(pasTemporel):
+
         return False
+
+
+
+    def calculerVitesse(self):
+        return
+
+
+    def notify(self):
+        return
+
+    def avancer(self, prochaine_voiture):
+        return
+
+    def set_vehicule_precedent(self, vehicule):
+        self.vehicule_precedent = vehicule
+        vehicule.set_vehicule_suivant(self)
+
+
+    def set_vehicule_suivant(self, vehicule):
+        self.vehicule_suivant.append(vehicule)
 
