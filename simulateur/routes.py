cae624bb67e6129dcf98f1967c0b5f3366a380b7
RastaMarcusLeRouge <javin.marc@gmail.com>
2016-04-26 16:57:14 +0200

    voie un peu

--- simulateur/routes.py
+++ simulateur/routes.py
@@ -2,54 +2,69 @@
 import coordonnees
 import controleAcces
 import vehicule
-    
+	
 
 class voie:
-    def __init__(self, direction, controleur_acces, troncon, feu, coordonnees_debut, coordonnees_fin):
-        self.intersectionsAccessibles = []
-        self.controleur_acces = controleur_acces
-        self.troncon = troncon
-        self.coordonnees_debut = coordonnees_debut
-        self.coordonnees_fin = coordonnees_fin
-        self.vehicules = []
-        self.directions = set([])
+	def __init__(self, direction, controleur_acces, troncon, feu, coordonnees_debut, coordonnees_fin):
+		self.intersectionsAccessibles = []
+		self.controleur_acces = controleur_acces
+		self.troncon = troncon
+		self.coordonnees_debut = coordonnees_debut
+		self.coordonnees_fin = coordonnees_fin
+		self.vehicules = []
+		self.directions = set([])
+		
+		
+		
+	def creer_vehicule(discourtois, longueur):
+		"""
+			Crée le véhicule
+			L'ajoute à la fin de la liste
+			L'ajoute à l'arbre
+		"""
+		prochaine_direction = "droite";
+		clio = vehicule(50, coordonnees, discourtois, longueur, self, prochaine_direction, null)
+		#ajouter à la fin de la liste
+		#ajouter à la fin de l'arbre
+		
+		
 
 
-    def mise_a_jour_controle_acces(self, temps, simulation_manager):
-         self.controleur_acces.notifie_temps(self, temps, simulation_manager)
+	def mise_a_jour_controle_acces(self, temps, simulation_manager):
+		 self.controleur_acces.notifie_temps(self, temps, simulation_manager)
 
-    def get_controleur_acces(self):
-        return self.controleur_acces
+	def get_controleur_acces(self):
+		return self.controleur_acces
 
-    def puis_je_passer(self, direction):
+	def puis_je_passer(self, direction):
+		return
 
+	#set de direction + probabilité de prendre cette direction en fonction des troncons accessibles et de leur proba
 
-    #set de direction + probabilité de prendre cette direction en fonction des troncons accessibles et de leur proba
+	#ajouter voiture
 
-    #ajouter voiture
+	#notification du véhicule en tête qui s'en va
 
-    #notification du véhicule en tête qui s'en va
+	# dernier élément
 
-    # dernier élément
-
-    def setTroncon(self, troncon):
-        self.troncon = troncon
+	def setTroncon(self, troncon):
+		self.troncon = troncon
 
 class troncon:
-    def __init__(self, tete, queue, probabilite_entree, longueur, coordonnees_):
-        self.tete = tete
-        self.queue = queue
-        self.probabilite_entree = probabilite_entree
-        self.longueur = longueur
-        self.voies = []
+	def __init__(self, tete, queue, probabilite_entree, longueur, coordonnees_):
+		self.tete = tete
+		self.queue = queue
+		self.probabilite_entree = probabilite_entree
+		self.longueur = longueur
+		self.voies = []
 
-    #trouver voie avec bonne direction
+	#trouver voie avec bonne direction
 
 
-    """
-    ainsi toujours poussé vers de nouveaux rivages
-    dans la nuit éternelle emporté sans retour
-    ne pourrons nous jamais sur l'océan des ages
-    jetez l'ancre un seul jour ?
-        AdL
-    """
+	"""
+	ainsi toujours poussé vers de nouveaux rivages
+	dans la nuit éternelle emporté sans retour
+	ne pourrons nous jamais sur l'océan des ages
+	jetez l'ancre un seul jour ?
+		AdL
+	"""
