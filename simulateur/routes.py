75959fe5d0b7d58053b22a2b2b04026673061a46
paul <compagnonpaul@hotmail.fr>
2016-04-26 15:09:17 +0200

    modif Nico

--- simulateur/routes.py
+++ simulateur/routes.py
@@ -3,19 +3,25 @@ from enum import Enum
 import coordonnees
 import controleAcces
 import vehicule
-
-
-class intersection:
     
 
 class voie:
-    def __init__(self, direction):
+    def __init__(self, direction, controleur_acces, troncon):
         self.intersectionsAccessibles = []
         self.direction = direction
-        self.vehicules = []
+        self.controleur_acces = controleur_acces
+        self.troncon = troncon
+
+    def mise_a_jour_controle_acces(self, temps, simulation_manager):
+         self.controleur_acces.notifie_temps(self, temps, simulation_manager)
+
+    def get_controleur_acces(self):
+        return self.controleur_acces
 
     def a_de_la_place(self):
         return
+
+
 
     def setTroncon(self, troncon):
         self.troncon = troncon
@@ -27,6 +33,8 @@ class troncon:
         self.probabilite_entree = probabilite_entree
         self.longueur = longueur
         self.voies = []
+
+    #trouver voie avec bonne direction
 
 class direction(Enum):
     gauche = 1
