aa54e05094772eff863b7ab422d72e5a17e2b004
paul <compagnonpaul@hotmail.fr>
2016-04-27 19:28:59 +0200

    proba

--- simulateur/routes.py
+++ simulateur/routes.py
@@ -6,7 +6,7 @@ import vehicule
 
 
 class voie:
-    def __init__(self, troncon, coordonnees_debut, coordonnees_fin, directions, trajectoire):
+    def __init__(self, troncon, coordonnees_debut, coordonnees_fin, directions, trajectoire, proba_entree, proba_dir):
         self.intersectionsAccessibles = []
         self.troncon = troncon
         self.coordonnees_debut = coordonnees_debut
@@ -14,12 +14,14 @@ class voie:
         self.vehicules = []
         self.trajectoire = trajectoire
         self.directions = directions
-        self.feux = feu;
+        self.proba_entree = proba_entree
+        self.proba_dir = proba_dir
+
 
 
     def creer_vehicule(self, discourtois, longueur):
         prochaine_direction = "droite"
-        clio = vehicule(50, coordonnees(0,0), discourtois, longueur, self, prochaine_direction, self.trajectoire, None)
+        clio = vehicule.vehicule(50, coordonnees.coordonnees(0,0), discourtois, longueur, self, prochaine_direction, self.trajectoire, None)
         self.ajouter_vehicule(self)
         dernier_vehicule = self.dernier_vehicule()
         clio.greffe_arbre(dernier_vehicule)
@@ -39,10 +41,10 @@ class voie:
     #notification du véhicule en tête qui s'en va
 
     def supprimer_vehicule(self, vehicule):
-        self.vehicules.delete(vehicule)
+        self.vehicules.remove(vehicule)
 
     def dernier_vehicule(self):
-        return self.vehicules(-1)
+        return self.vehicules[-1]
 
     def setTroncon(self, troncon):
         self.troncon = troncon
@@ -56,62 +58,105 @@ class voie:
     def est_passant(self, direction):
         return self.troncon.getFeu(direction).est_passant()
 
+    def get_proba_dir(self, direction):
+        return self.proba_dir.get(direction)
+
 
 class troncon:
-    const_largeur_voie = 350 #centimètres
+    const_largeur_voie = 350 #centimètres, largeur standard d'une voie en France
     def __init__(self, tete, queue, coordonnees_debut, coordonnees_fin, proba_dir_sens1, proba_dir_sens2):  #sens1 : gauche vers droite, bas vers haut
         self.tete = tete #en haut ou à droite
         self.queue = queue #en bas ou à gauche
-        self.coordonees_debut = coordonnees_debut
-        self.coordonees_fin = coordonnees_fin
-        self.longueur = math.sqrt((self.coordonnees_fin.x-self.coordonnees_debut.x)**2 + (self.coordonnees_fin.y-self.coordonnees_debut.y)**2)
-        self.trajectoire = coordonnees((self.coordonnees_fin.x-self.coordonnees_debut.x)/self.longueur, (self.coordonnees_fin.y-self.coordonnees_debut.y)/self.longueur)
+        self.coordonnees_debut = coordonnees_debut
+        self.coordonnees_fin = coordonnees_fin
+        self.longueur = self.coordonnees_debut.norme(coordonnees_fin)
+        self.trajectoire = coordonnees.coordonnees((self.coordonnees_fin.x-self.coordonnees_debut.x)/self.longueur, (self.coordonnees_fin.y-self.coordonnees_debut.y)/self.longueur)
         self.proba_dir_sens1 = proba_dir_sens1
         self.proba_dir_sens2 = proba_dir_sens2
         self.directions_sens1 = self.proba_dir_sens1.keys()
         self.directions_sens2 = self.proba_dir_sens2.keys()
         self.voies_sens1 = []
         self.voies_sens2 = []
-        self.dir_voies = {}
+        self.dir_voies_sens1 = {}
+        self.dir_voies_sens2 = {}
         self.dir_feu = {}
 
-        for direction in self.directions :
-            self.dir_feu[direction] = feu(self.tete)
+        for direction in self.directions_sens1 :
+            self.dir_feu[direction] = feu.feu(self.tete)
 
     def creer_voie(self, directions, sens):  #on crée les voies de l'intérieur vers l'extérieur dans les deux sens, l'utilisateur fera donc attention aux directions qu'il passe en paramètre (gauche d'abord)
+        coordonnees_debut = None
+        coordonnees_fin = None
         if( sens == "sens1") :
             if(self.coordonnees_debut.x == self.coordonnees_fin.x):
-                coordonnees_debut = coordonnees(self.coordonees_debut.x + (len(self.voies_sens1) + 0,5)*self.const_largeur_voie, self.coordonnees_debut.y )
-                coordonnees_fin = coordonnees(self.coordonees_debut.x + (len(self.voies_sens1) + 0,5)*self.const_largeur_voie, self.coordonnees_fin.y )
+                coordonnees_debut = coordonnees.coordonnees(self.coordonnees_debut.x + (len(self.voies_sens1) + 0,5)*self.const_largeur_voie, self.coordonnees_debut.y )
+                coordonnees_fin = coordonnees.coordonnees(self.coordonnees_debut.x + (len(self.voies_sens1) + 0,5)*self.const_largeur_voie, self.coordonnees_fin.y )
             if (self.coordonnees_debut.y == self.coordonnees_fin.y):
-                coordonnees_debut = coordonnees(self.coordonees_debut.x,self.coordonnees_debut.y + (len(self.voies_sens1) + 0, 5) * self.const_largeur_voie)
-                coordonnees_fin = coordonnees(self.coordonees_fin.x ,self.coordonnees_fin.y + (len(self.voies_sens1) + 0, 5) * self.const_largeur_voie)
-            self.voies.append(voie(self, coordonnees_debut, coordonnees_fin, directions, self.trajectoire))
+                coordonnees_debut = coordonnees.coordonnees(self.coordonnees_debut.x,self.coordonnees_debut.y - (len(self.voies_sens1) + 0, 5)*self.const_largeur_voie)
+                coordonnees_fin = coordonnees.coordonnees(self.coordonnees_fin.x ,self.coordonnees_fin.y - (len(self.voies_sens1) + 0, 5)*self.const_largeur_voie)
+
+            proba_dir = {}
+            proba_sum = 0
+            proba_entree = 0
+            for direction in directions :
+                proba_entree = proba_entree + self.proba_dir_sens1[direction]/(len(self.dir_voies_sens1[direction]) +1) #la nouvelle voie n'est pas encore dans la liste
+                proba_dir[direction] = self.proba_dir_sens1.get[direction]
+                proba_sum = proba_sum + self.proba_dir_sens1.get[direction]
+
+            for direction in directions :
+                proba_dir[direction] = proba_dir.get(direction)/proba_sum
+
+            v = voie(self, coordonnees_debut, coordonnees_fin, directions, self.trajectoire, proba_entree, proba_dir)
+
+            for direction in directions:
+                self.dir_voies_sens1[direction] = [self.dir_voies_sens1.get(direction)] + [v]
 
         if (sens == "sens2"):
             if (self.coordonnees_debut.x == self.coordonnees_fin.x):
-                coordonnees_debut = coordonnees(self.coordonees_debut.x - (len(self.voies_sens1) + 0, 5) * self.const_largeur_voie, self.coordonnees_debut.y)
-                coordonnees_fin = coordonnees(self.coordonees_debut.x - (len(self.voies_sens1) + 0, 5) * self.const_largeur_voie, self.coordonnees_fin.y)
+                coordonnees_debut = coordonnees.coordonnees(self.coordonnees_debut.x - (len(self.voies_sens2) + 0, 5)*self.const_largeur_voie, self.coordonnees_debut.y)
+                coordonnees_fin = coordonnees.coordonnees(self.coordonnees_debut.x - (len(self.voies_sens2) + 0, 5)*self.const_largeur_voie, self.coordonnees_fin.y)
             if (self.coordonnees_debut.y == self.coordonnees_fin.y):
-                coordonnees_debut = coordonnees(self.coordonees_debut.x, self.coordonnees_debut.y - (len(self.voies_sens1) + 0, 5) * self.const_largeur_voie)
-                coordonnees_fin = coordonnees(self.coordonees_fin.x, self.coordonnees_fin.y - (len(self.voies_sens1) + 0, 5) * self.const_largeur_voie)
-            self.voies.append(voie(self, coordonnees_debut, coordonnees_fin, directions, self.trajectoire))
+                coordonnees_debut = coordonnees.coordonnees(self.coordonnees_debut.x, self.coordonnees_debut.y + (len(self.voies_sens2) + 0, 5)*self.const_largeur_voie)
+                coordonnees_fin = coordonnees.coordonnees(self.coordonnees_fin.x, self.coordonnees_fin.y + (len(self.voies_sens2) + 0, 5)*self.const_largeur_voie)
+            self.voies_sens2.append(voie(self, coordonnees_debut, coordonnees_fin, directions, self.trajectoire))
 
+            proba_dir = {}
+            proba_sum = 0
+            proba_entree = 0
+            for direction in directions:
+                proba_entree = proba_entree + self.proba_dir_sens2[direction]/(len(self.dir_voies_sens2[direction]) + 1)  # la nouvelle voie n'est pas encore dans la liste
+                proba_dir[direction] = self.proba_dir_sens2.get[direction]
+                proba_sum = proba_sum + self.proba_dir_sens2.get[direction]
 
+            for direction in directions:
+                proba_dir[direction] = proba_dir.get(direction)/proba_sum
+
+            v = voie(self, coordonnees_debut, coordonnees_fin, directions, self.trajectoire, proba_entree, proba_dir)
+            for direction in directions:
+                self.dir_voies_sens2[direction] = [self.dir_voies_sens2.get(direction)] + [v]
+
+    # end creer voie
 
     #trouver voie avec bonne direction
-    def trouver_voie_direction(self, direction):
+    def trouver_voie_direction(self, direction, sens):
         voies_possibles = []
-        for voie in self.voies:
-            if(voie.direction_possible(direction)):
-                voies_possibles.append(voie)
+        if(sens == "sens1") :
+            for voie in self.voies_sens1:
+                if(voie.direction_possible(direction)):
+                    voies_possibles.append(voie)
+
+        if(sens == "sens2") :
+            for voie in self.voies_sens2:
+                if(voie.direction_possible(direction)):
+                    voies_possibles.append(voie)
+
         return voies_possibles
 
     def getFeu(self, direction):
         if(direction in self.dir_feu.keys()):
             return self.dir_feu.get(direction)
         else:
-            self.dir_feu[direction] = feu(self.tete)
+            self.dir_feu[direction] = feu.feu(self.tete)
             return self.dir_feu.get(direction)
 
 
