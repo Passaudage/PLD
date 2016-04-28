95db6288f1df40be284fe7c76e64c922e71c784b
Quentin <quentin.labernia@insa-lyon.fr>
2016-04-28 22:44:27 +0200

    Modifs

--- simulateur/Fenetre.py
+++ simulateur/Fenetre.py
@@ -2,32 +2,37 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-import cairo
+from SimulationManager import *
+from Visualisateur import *
 
 class Fenetre(Gtk.Window):
-	"""
-		Classe de la fenêtre d'affichage
-			# @author : Quentin
-	"""
+    """
+        Classe de la fenêtre d'affichage
+            # @author : Quentin
+    """
 
-	def __init__(self):
-		Gtk.Window.__init__(self)
-		self.set_title("Visualisation")
-		self.set_default_size(800,600)
+    def __init__(self):
+        Gtk.Window.__init__(self)
+        self.set_title("Visualisation")
+        self.set_default_size(800,600)
 
-		self.connect('delete-event', self.quit)
-		
-		zone_dessin = Gtk.DrawingArea()
-		zone_dessin.connect('draw', self.dessiner_tout)
-		self.add(zone_dessin)
-		self.show_all()
+        self.connect('delete-event', self.quit)
+        
+        self.def_visual()
 
-	def dessiner_tout(self, widget, cairo_context):
-		print("Dessin !")
+        self.add(self.visual.zone_dessin)
+        self.show_all()
 
-	def quit(self, a, b):
-		# voir la doc, je ne sais pas à quoi correspondent ces deux arguments...
-		Gtk.main_quit(a, b)
+    def def_visual(self):
+
+        grain = 100
+
+        self.sim = SimulationManager(grain)
+        self.visual = Visualisateur(self.sim)
+
+    def quit(self, a, b):
+        # voir la doc, je ne sais pas à quoi correspondent ces deux arguments...
+        Gtk.main_quit(a, b)
 
 fenetre = Fenetre()
 Gtk.main()
--- simulateur/Visualisateur.py
+++ simulateur/Visualisateur.py
@@ -1,16 +1,68 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+from Coordonnees import *
+
+import Vehicule
 
 class Visualisateur: 
     """
         Classe de visualisation du traffic
+            # zone_dessin : contexte pour dessiner via cairo
+            # simulateur : permet de récupérer les infos sur les voitures
             # @author : Quentin
     """
-    
+
     def __init__(self, simulateur):
 
         self.grain = 1 # en ticks
         self.simulateur = simulateur
+        self.zone_dessin = Gtk.DrawingArea()
+
+        self.zone_dessin.connect('draw', self.dessiner_tout)
+
+    def dessiner_tout(self, widget, cairo_context):
+        self.cairo_context = cairo_context
+
+        self.dessiner_voitures()
+
+        widget.queue_draw()
+
+    def dessiner_voitures(self):
+        """
+            Cette  méthode dessine l'ensemble des voitures
+        """
+        # couleur d'une voiture
+        self.cairo_context.set_source_rgba(0, 0, 70, 0.5)
+
+        coord_test = Coordonnees(100, 100)
+        orientation = 0
+        longueur = 50
+
+        self.dessiner_voiture(coord_test, orientation, longueur);
+        self.dessiner_voiture(coord_test, orientation, longueur);
+
+    def dessiner_voiture(self, coord, orientation, longueur):
+
+        self.cairo_context.save()
+
+        print("dessin !")
+        #self.cairo_context.arc(0, 0, 50, 0, 0)
+        self.cairo_context.translate(coord.x, coord.y)
+        self.cairo_context.rotate(orientation) # en degrés
+
+        self.cairo_context.rectangle(0, 0, Vehicule.Vehiculelargeur, longueur)
+        self.cairo_context.fill()
+
+        self.cairo_context.restore()
+
+        return
 
     def dessiner_voirie(self):
+        # on récupère les intersections, tronçons, etc.
+        return
 
+    def dessiner_intersection(self):
 
-
+        return
