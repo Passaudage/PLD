076a305eedb1e4a329206ad9816c5086c06a23c8
Quentin <quentin.labernia@insa-lyon.fr>
2016-05-02 19:23:04 +0200

    ajout menu apprentissage

--- simulateur/Fenetre.py
+++ simulateur/Fenetre.py
@@ -9,8 +9,17 @@ import main
 import jacky
 import DoubleCarrefour
 import TripleCarrefour
+import Apprentissage
 
 import sys
+
+def get_simulateur():
+
+    #sim = jacky.charger_simulateur()
+    sim = DoubleCarrefour.charger_simulateur()
+    #~ sim = TripleCarrefour.charger_simulateur()
+
+    return sim
 
 class Fenetre(Gtk.ApplicationWindow):
     """
@@ -32,8 +41,8 @@ class Fenetre(Gtk.ApplicationWindow):
         self.connect('delete-event', self.quit)
 
 
-    def quit(self, action, parameter):
-        self.app.quitter_callback(action, parameter)
+    def quit(self, action, parametre):
+        self.app.quitter_callback(action, parametre)
 
 
 class Application(Gtk.Application):
@@ -46,13 +55,17 @@ class Application(Gtk.Application):
         self.win = Fenetre(self)
         self.win.show_all()
 
-    def simuler_callback(self, action, parameter):
+    def simuler_callback(self, action, parametre):
         self.def_visual()
 
-    def quitter_callback(self, action, parameter):
+    def quitter_callback(self, action, parametre):
         if self.visual is not None:
             self.visual.notifier_fin()
         sys.exit()
+
+    def apprentissage_callback(self, action, parametre):
+        sim = get_simulateur()
+        self.apprentissage = Apprentissage.Apprentissage(sim)
 
     def do_startup(self):
         Gtk.Application.do_startup(self)
@@ -66,18 +79,19 @@ class Application(Gtk.Application):
         simuler_action.connect("activate", self.simuler_callback)
         self.add_action(simuler_action)
 
+        # action "apprentissage" de la barre de menu
+        apprentissage_action = Gio.SimpleAction.new("apprentissage", None)
+        apprentissage_action.connect("activate", self.apprentissage_callback)
+        self.add_action(apprentissage_action)
+
         # action "quitter" de la barre de menu
         quitter_action = Gio.SimpleAction.new("quitter", None)
         quitter_action.connect("activate", self.quitter_callback)
         self.add_action(quitter_action)
 
     def def_visual(self):
-        #~ self.sim = jacky.charger_simulateur()
 
-        self.sim = jacky.charger_simulateur()
-
-        #~ self.sim = DoubleCarrefour.charger_simulateur()
-        #~ self.sim = TripleCarrefour.charger_simulateur()
+        self.sim = get_simulateur()
 
         self.visual = Visualisateur.Visualisateur(self.sim, Fenetre.taille_x, Fenetre.taille_y)
         self.visual.demarrer_simulation()
